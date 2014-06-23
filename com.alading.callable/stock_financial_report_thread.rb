214d669957f1a6aea0c5e92c4b1baf2879b903a0
jiang <892334874@qq.com>
2014-06-24 07:27:27 +0800

    commit  callable

---
+++ com.alading.callable/stock_financial_report_thread.rb
@@ -0,0 +1,8 @@
+class StockFinancialReportThread
+  # http://vip.stock.finance.sina.com.cn/corp/go.php/vFD_BalanceSheet/stockid/600073/ctrl/part/displaytype/4.phtml
+  # http://vip.stock.finance.sina.com.cn/corp/go.php/vFD_ProfitStatement/stockid/600073/ctrl/part/displaytype/4.phtml
+  # http://vip.stock.finance.sina.com.cn/corp/go.php/vFD_CashFlow/stockid/600073/ctrl/part/displaytype/4.phtml
+
+
+  # http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=1&num=40&sort=symbol&asc=1&node=new_fdc&symbol=&_s_r_a=page
+end
