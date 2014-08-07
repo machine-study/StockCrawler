9ee5c49d9b6a568cb88285b88fe347921377d3bb
jiang <892334874@qq.com>
2014-08-07 23:42:04 +0800

    quarterly data caculate

--- data_process/single_quarter_report_process.rb
+++ data_process/single_quarter_report_process.rb
@@ -17,9 +17,19 @@ class SingleQuarterReportProcess
     stock_map.each_value do |stock_array|
       for i in 0...stock_array.length-1
         profit_statement_report = ProfitStatementReport.new
-        profit_statement_report.code = stock_array[i].code
-        profit_statement_report.gross_revenue = stock_array[i].gross_revenue-stock_array[i+1].gross_revenue
+        profit_statement_report.instance_values.each do |name|
+          if stock_array[i].instance_variable_get(name).kind_of? Numeric
+            profit_statement_report.instance_variable_set(name, stock_array[i].instance_variable_get(name)-stock_array[i+1].instance_variable_get(name))
+          else
+            profit_statement_report.instance_variable_set(name, stock_array[i].instance_variable_get(name))
+          end
 
+        end
+        earning_quarterly = stock_array[i].net_profit-stock_array[i+1].net_profit
+        profit_statement_report.basic_earnings_per_share_yuan=earning_quarterly/(stock_array[i].net_profit/stock_array[i].basic_earnings_per_share_yuan)
+        profit_statement_report.diluted_earnings_per_share_yuan = earning_quarterly/(stock_array[i].net_profit/stock_array[i].diluted_earnings_per_share_yuan)
+        profit_statement_report.is_single_quarter=true
+        profit_statement_report.save
       end
     end
   end
--- db/migrate/008_add_is_single_quarter_to_report.rb
+++ db/migrate/008_add_is_single_quarter_to_report.rb
@@ -3,5 +3,19 @@ class AddIsSingleQuarterToReport
     add_column :balance_sheet_reports, :is_single_quarter, :boolean
     add_column :profit_statement_reports, :is_single_quarter, :boolean
     add_column :cash_flow_reports, :is_single_quarter, :boolean
+
+    remove_index :balance_sheet_reports, [:symbol, :report_date]
+    remove_index :profit_statement_reports, [:symbol, :report_date]
+    remove_index :cash_flow_reports, [:symbol, :report_date]
+
+    remove_index :balance_sheet_reports, [:code, :report_date]
+    remove_index :profit_statement_reports, [:code, :report_date]
+    remove_index :cash_flow_reports, [:code, :report_date]
+
+    add_index :balance_sheet_reports, [:code, :report_date, :is_single_quarter], :unique => true
+    add_index :profit_statement_reports, [:code, :report_date, :is_single_quarter], :unique => true
+    add_index :cash_flow_reports, [:code, :report_date, :is_single_quarter], :unique => true
+
+
   end
 end
