7c5bb7387903be355acab887db2a400e8592752d
jiang <892334874@qq.com>
2014-08-09 00:45:03 +0800

    quarterly data caculate

--- data_process/single_quarter_report_process.rb
+++ data_process/single_quarter_report_process.rb
@@ -2,10 +2,10 @@ class SingleQuarterReportProcess
   def get_profit_statement_map(start_time, end_time)
     stock_map = Hash.new
     ProfitStatementReport.select.where(report_date: start_time..end_time).find_each do |stock|
-      if stock_map[stock.code]==nil
-        stock_map[stock.code]=Array.new
+      if stock_map[stock.code+stock.report_date.year.to_s]==nil
+        stock_map[stock.code+stock.report_date.year.to_s]=Array.new
       end
-      stock_map[stock.code]<<stock
+      stock_map[stock.code+stock.report_date.year.to_s]<<stock
     end
     stock_map.each_value do |stock_array|
       stock_array.sort! { |a, b| b.report_date <=> a.report_date }
@@ -15,15 +15,26 @@ class SingleQuarterReportProcess
   def set_quarterly_profit_statement(stock_map)
     quarter_stock_array = Array.new
     stock_map.each_value do |stock_array|
+      if stock_array[0].report_date.to_s.include?('03-31')
+        profit_statement_report = ProfitStatementReport.new
+        profit_statement_report.instance_values.each do |name|
+          profit_statement_report.instance_variable_set(name, stock_array[0].instance_variable_get(name))
+        end
+        profit_statement_report.is_single_quarter=true
+        profit_statement_report.save
+        break
+      end
       for i in 0...stock_array.length-1
         profit_statement_report = ProfitStatementReport.new
+        if (stock_array[i].report_date-stock_array[i+1].report_date)/(60*60*24)>100
+          next
+        end
         profit_statement_report.instance_values.each do |name|
           if stock_array[i].instance_variable_get(name).kind_of? Numeric
             profit_statement_report.instance_variable_set(name, stock_array[i].instance_variable_get(name)-stock_array[i+1].instance_variable_get(name))
           else
             profit_statement_report.instance_variable_set(name, stock_array[i].instance_variable_get(name))
           end
-
         end
         earning_quarterly = stock_array[i].net_profit-stock_array[i+1].net_profit
         profit_statement_report.basic_earnings_per_share_yuan=earning_quarterly/(stock_array[i].net_profit/stock_array[i].basic_earnings_per_share_yuan)
--- db/migrate/008_add_is_single_quarter_to_report.rb
+++ db/migrate/008_add_is_single_quarter_to_report.rb
@@ -1,20 +1,10 @@
-class AddIsSingleQuarterToReport
+class AddIsSingleQuarterToReport< ActiveRecord::Migration
   def change
-    add_column :balance_sheet_reports, :is_single_quarter, :boolean
-    add_column :profit_statement_reports, :is_single_quarter, :boolean
-    add_column :cash_flow_reports, :is_single_quarter, :boolean
 
-    remove_index :balance_sheet_reports, [:symbol, :report_date]
-    remove_index :profit_statement_reports, [:symbol, :report_date]
-    remove_index :cash_flow_reports, [:symbol, :report_date]
 
-    remove_index :balance_sheet_reports, [:code, :report_date]
-    remove_index :profit_statement_reports, [:code, :report_date]
-    remove_index :cash_flow_reports, [:code, :report_date]
-
-    add_index :balance_sheet_reports, [:code, :report_date, :is_single_quarter], :unique => true
-    add_index :profit_statement_reports, [:code, :report_date, :is_single_quarter], :unique => true
-    add_index :cash_flow_reports, [:code, :report_date, :is_single_quarter], :unique => true
+    add_index :balance_sheet_reports, [:code, :report_date, :is_single_quarter], :unique => true,:name=>'quarter_index'
+    add_index :profit_statement_reports, [:code, :report_date, :is_single_quarter], :unique => true,:name=>'quarter_index'
+    add_index :cash_flow_reports, [:code, :report_date, :is_single_quarter], :unique => true,:name=>'quarter_index'
 
 
   end
